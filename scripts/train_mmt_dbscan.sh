ec2bf7aa8317a23627454661f5c42ad15b16e303
ChienHsuan <68268840+ChienHsuan@users.noreply.github.com>
2021-11-21 21:42:11 +0800

    Original repository was modified to be compatible with local environment

--- scripts/train_mmt_dbscan.sh
+++ scripts/train_mmt_dbscan.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-SOURCE=$1
-TARGET=$2
-ARCH=$3
+SOURCE="dukemtmc"
+TARGET="market1501"         # market1501  dukemtmc
+ARCH="msanet_pos"
 
-if [ $# -ne 3 ]
-  then
-    echo "Arguments error: <SOURCE> <TARGET> <ARCH>"
-    exit 1
-fi
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+export PYTHONPATH=$PYTHONPATH:`pwd`
+CUDA_VISIBLE_DEVICES=4 \
 python examples/mmt_train_dbscan.py -ds ${SOURCE} -dt ${TARGET} -a ${ARCH} \
 	--num-instances 4 --lr 0.00035 --iters 400 -b 64 --epochs 40 \
 	--soft-ce-weight 0.5 --soft-tri-weight 0.8 --dropout 0 --lambda-value 0 \
-	--init-1 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-1/model_best.pth.tar \
-	--init-2 logs/${SOURCE}TO${TARGET}/${ARCH}-pretrain-2/model_best.pth.tar \
-	--logs-dir logs/${SOURCE}TO${TARGET}/${ARCH}-MMT-DBSCAN
-	# --rr-gpu
+	--height 192 --width 96 --features 512 \
+	--init-1 /home/lab314/HDD/Hsuan/models/peer_networks/dukemtmc/msanet_pos/1/model.pth.tar-100 \
+	--init-2 /home/lab314/HDD/Hsuan/models/peer_networks/dukemtmc/msanet_pos/1/model.pth.tar-100 \
+	--data-dir /home/lab314/HDD/Dataset \
+	--logs-dir /home/lab314/HDD/Hsuan/MMT/log \
+	--eval-step 1 \
+	--print-freq 50
